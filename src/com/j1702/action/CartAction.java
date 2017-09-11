6a7432df1e12c763143dad4c943826f6aba0473e
jfh389987366 <389987366@qq.com>
2017-09-11 16:10:30 +0800

    aaaa

--- src/com/j1702/action/CartAction.java
+++ src/com/j1702/action/CartAction.java
@@ -11,16 +11,19 @@ public class CartAction {
 
 	public void addCart(String userName1,String goodsName1,Double goodsPrice1,Integer goodsNumber1,String goodsUrl1,String goodsIntro1){
 		CartDao cd=new CartDao();
+		//aaaa
 		cd.add(userName1,goodsName1,goodsPrice1,goodsNumber1,goodsUrl1,goodsIntro1);
 	}
 
 	public void deleteCart(String username,String goodsname){
 		CartDao cd=new CartDao();
+		//aaaa
 		cd.deleteCart(username, goodsname);
 	}
 	
 	public void deleteAll(String userName1){
 		CartDao cd=new CartDao();
+		//aaaaa
 		cd.deleteAll(userName1);
 	}
 	//����ָ���û��ڹ��ﳵ�е�����
