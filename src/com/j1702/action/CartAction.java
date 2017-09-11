8511e96fb80e9486a7ee7b82ee1f3f64103f4428
rimi <rimi@192.168.2.1>
2017-09-11 15:53:43 +0800

    test

--- src/com/j1702/action/CartAction.java
+++ src/com/j1702/action/CartAction.java
@@ -8,34 +8,34 @@ import com.j1702.model.Cart;
 
 public class CartAction {
 
-	//添加购物车数据
+
 	public void addCart(String userName1,String goodsName1,Double goodsPrice1,Integer goodsNumber1,String goodsUrl1,String goodsIntro1){
 		CartDao cd=new CartDao();
 		cd.add(userName1,goodsName1,goodsPrice1,goodsNumber1,goodsUrl1,goodsIntro1);
 	}
-	//删除指定用户的指定数据
+
 	public void deleteCart(String username,String goodsname){
 		CartDao cd=new CartDao();
 		cd.deleteCart(username, goodsname);
 	}
-	//删除指定用户在购物车中的所有数据
+	
 	public void deleteAll(String userName1){
 		CartDao cd=new CartDao();
 		cd.deleteAll(userName1);
 	}
-	//查找指定用户在购物车中的数据
+	//锟斤拷锟斤拷指锟斤拷锟矫伙拷锟节癸拷锟斤车锟叫碉拷锟斤拷锟斤拷
 	public List<Cart> showCart(String userName1){
 		CartDao cd=new CartDao();
 		List<Cart> list=new ArrayList<>();
 		list=cd.show(userName1);
 		return list;
 	}
-	//查询是否有指定商品
+	//锟斤拷询锟角凤拷锟斤拷指锟斤拷锟斤拷品
 	public Boolean isGoodsExist(String goodsname,String username){
 		CartDao cd=new CartDao();
 		return cd.isGoodsExist(goodsname,username);
 	}
-	//修改指定商品的数量
+	//锟睫革拷指锟斤拷锟斤拷品锟斤拷锟斤拷锟斤拷
 	public void changeNumber(Integer goodsnumber,String username){
 		CartDao cd=new CartDao();
 		cd.changeNumber(goodsnumber, username);
